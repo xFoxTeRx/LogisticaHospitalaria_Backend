f69993b6a5518c5a6c279d2df93cf27a578f1a86
xFoxTeRx <r44oben@gmail.com>
2026-05-05 18:06:30 -0400

    hecho

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // 1. CONFIGURACIÓN DEL PUERTO PARA RAILWAY (Añade esto aquí)
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0:{port}");
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddDbContext<LogisticaHospitalariaContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("LogisticaHospitalariaContext")
